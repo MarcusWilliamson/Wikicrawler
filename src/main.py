bf21e67e24391e24f5c8b3a6dc92db16853e0b1a
Marcus Williamson <hamawill@ucsc.edu>
2025-05-19 22:27:22 -0700

    Function to check for valid links

--- src/main.py
+++ src/main.py
@@ -4,14 +4,29 @@ from bs4 import BeautifulSoup
 
 start_url, target_url = sys.argv[1], sys.argv[2]
 
+def is_valid_link(url):
+    print("Checking:", url)
+    if url is None:
+        return False
+    if '/wiki/' not in url:
+        return False
+    if '#' in url:
+        return False
+    return True
+
 def get_all_links(url):
 
     request = requests.get(url)
     soup = BeautifulSoup(request.content, 'html.parser')
 
     links = []
-    for link in soup.find_all('a'):
-        links.append(link.get('href'))
+    for href in soup.find_all('a'):
+        link = href.get('href')
+        if is_valid_link(link):
+            links.append(link)
     return links
 
 print(get_all_links(start_url))
+
+# def BFS(source, destination):
+#     queue = []
